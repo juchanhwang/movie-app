8b770da6efafd4ab10a5d3bb2fc71bf357f22ae2
juchanhwang <ghkdwncks96@gmail.com>
2019-05-08 18:26:40 +0900

    add: React Helmet on Detail, Home, TV, Search Containers

--- src/Routes/Home/HomePresenter.js
+++ src/Routes/Home/HomePresenter.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import Helmet from 'react-helmet'
 import Section from 'Components/Section';
 import Loader from '../../Components/Loader';
 import Message from '../../Components/Message';
@@ -11,7 +12,12 @@ const Container = styled.div`
 `;
 
 const HomePresenter = ({ nowPlaying, popular, upComing, loading, error }) =>
-  loading ? <Loader /> :
+  <>
+    <Helmet>
+      <title> Movies | Nomflix</title>
+    </Helmet>
+
+    loading ? <Loader /> :
     <Container>
       {nowPlaying && nowPlaying.length > 0 && (
         <Section title="Now Playing">
@@ -60,6 +66,7 @@ const HomePresenter = ({ nowPlaying, popular, upComing, loading, error }) =>
       )}
       {error && <Message color="#e74c3c;" text={error} />}
     </Container>;
+  </>
 
 HomePresenter.propTypes = {
   nowPlaying: PropTypes.array,
--- src/Routes/TV/TVPresenter.js
+++ src/Routes/TV/TVPresenter.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import Helmet from 'react-helmet';
 import Section from '../../Components/Section';
 import Loader from '../../Components/Loader';
 import Message from '../../Components/Message';
@@ -8,11 +9,15 @@ import Poster from '../../Components/Poster';
 
 
 const Container = styled.div`
-  padding: 0px 10px;
+  padding: 20px;
 `;
 
 const TVPresenter = ({ topRated, popular, airingToday, loading, error }) =>
-  loading ? (<Loader />) : (
+  <>
+    <Helmet>
+      <title> TV | Nomflix </title>
+    </Helmet>
+    loading ? (<Loader />) : (
     <Container>
       {topRated && topRated.length > 0 && (
         <Section title="Top Rated Shows">
@@ -58,7 +63,8 @@ const TVPresenter = ({ topRated, popular, airingToday, loading, error }) =>
       )}
       {error && <Message color="#e74c3c;" text={error} />}
     </Container>
-  );
+    );
+  </>
 
 
 TVPresenter.propTypes = {
