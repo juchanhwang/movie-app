46e104e75a212846c0ff09fa5377cd183c431d25
juchanhwang <ghkdwncks96@gmail.com>
2019-05-08 18:40:38 +0900

    refactoring: 웹 하면에 노출되는 문자열이 노출되지 않게 수정

--- src/Routes/TV/TVPresenter.js
+++ src/Routes/TV/TVPresenter.js
@@ -17,53 +17,53 @@ const TVPresenter = ({ topRated, popular, airingToday, loading, error }) =>
     <Helmet>
       <title> TV | Nomflix </title>
     </Helmet>
-    loading ? (<Loader />) : (
-    <Container>
-      {topRated && topRated.length > 0 && (
-        <Section title="Top Rated Shows">
-          {topRated.map(show => (
-            <Poster
-              key={show.id}
-              id={show.id}
-              imageUrl={show.poster_path}
-              title={show.original_name}
-              rating={show.vote_average}
-              year={show.first_air_date.substring(0, 4)}
-            />
-          ))}
-        </Section>
-      )}
-      {popular && popular.length > 0 && (
-        <Section title="Popular Shows">
-          {popular.map(show => (
-            <Poster
-              key={show.id}
-              id={show.id}
-              imageUrl={show.poster_path}
-              title={show.original_name}
-              rating={show.vote_average}
-              year={show.first_air_date.substring(0, 4)}
-            />
-          ))}
-        </Section>
-      )}
-      {airingToday && airingToday.length > 0 && (
-        <Section title="Airing Today">
-          {airingToday.map(show => (
-            <Poster
-              key={show.id}
-              id={show.id}
-              imageUrl={show.poster_path}
-              title={show.original_name}
-              rating={show.vote_average}
-              year={show.first_air_date.substring(0, 4)}
-            />
-          ))}
-        </Section>
-      )}
-      {error && <Message color="#e74c3c;" text={error} />}
-    </Container>
-    );
+    {loading ? <Loader /> : (
+      <Container>
+        {topRated && topRated.length > 0 && (
+          <Section title="Top Rated Shows">
+            {topRated.map(show => (
+              <Poster
+                key={show.id}
+                id={show.id}
+                imageUrl={show.poster_path}
+                title={show.original_name}
+                rating={show.vote_average}
+                year={show.first_air_date.substring(0, 4)}
+              />
+            ))}
+          </Section>
+        )}
+        {popular && popular.length > 0 && (
+          <Section title="Popular Shows">
+            {popular.map(show => (
+              <Poster
+                key={show.id}
+                id={show.id}
+                imageUrl={show.poster_path}
+                title={show.original_name}
+                rating={show.vote_average}
+                year={show.first_air_date.substring(0, 4)}
+              />
+            ))}
+          </Section>
+        )}
+        {airingToday && airingToday.length > 0 && (
+          <Section title="Airing Today">
+            {airingToday.map(show => (
+              <Poster
+                key={show.id}
+                id={show.id}
+                imageUrl={show.poster_path}
+                title={show.original_name}
+                rating={show.vote_average}
+                year={show.first_air_date.substring(0, 4)}
+              />
+            ))}
+          </Section>
+        )}
+        {error && <Message color="#e74c3c;" text={error} />}
+      </Container>
+    )}
   </>
 
 
